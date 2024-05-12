2a375f689f820b447aa32fa0c9c1b502cedaa1a2
Reaien <ivanduarteherrera@gmail.com>
2024-05-12 18:22:40 -0400

    api_rest created despachos/

--- api/models.py
+++ api/models.py
@@ -2,26 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Camion(models.Model):
-    patente_camion = models.CharField(max_length=6, null=True)
+    patente_camion = models.CharField(max_length=6, null=True, unique=True)
     conductor = models.CharField(max_length=30, null=True)
 
     class Meta:
         db_table = 'Camiones'
 
     def __str__(self):
-        return self.patente_camion 
+        return f"Patente Camión:{self.patente_camion}"
 
 class Despacho(models.Model):
-    fecha = models.DateField( null=True)
-    patente_camion = models.ForeignKey(Camion, on_delete=models.PROTECT, null=True)
+    fecha_despacho = models.DateField( null=True)
+    patente_camion = models.ForeignKey(Camion, on_delete=models.PROTECT, null=True,  to_field='patente_camion')
     intento = models.IntegerField(default=0)
-    entrgado = models.BooleanField(default=False)
+    entregado = models.BooleanField(default=False)
+    id_compra = models.IntegerField(null=True)
+    direccion_compra = models.CharField(max_length=80, null=True)
+    valor_compra = models.IntegerField(null=True)
 
     class Meta:
         db_table = 'Despacho'
 
 
-
+    def __str__(self):
+        return f"Despacho ID:{self.pk}"
 
 
 class Resumen_Despacho(models.Model):
@@ -30,3 +34,6 @@ class Resumen_Despacho(models.Model):
 
     class Meta:
         db_table = 'Resumen_Despacho'
+
+    def __str__(self):
+        return f"Resumen Despacho ID:{self.pk}"
